5a829df3235402c63021d9ea83dcd6d0e2ca7d18
Emirlan <you@example.com>
2022-09-21 16:11:06 +0600

    dastan

--- molodym/settings.py
+++ molodym/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-py@l18=4^!29tbp@n)4_=o#6hr3rc3-07^1unxujx9pc9nmvkx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -76,10 +77,22 @@ WSGI_APPLICATION = 'molodym.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bilalovc_db',
+        'USER': 'bilalovc_db',
+        'PASSWORD': 'eHh=8x.oan[',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
